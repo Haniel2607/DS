cec7ae8c3576e2bb4869847b5bc1b12e09c0c8c9
Haniel2607 <148817243+Haniel2607@users.noreply.github.com>
2024-03-15 22:01:47 -0300

    Add files via upload

---
+++ sortearFrutas/sortearFrutas/sortearFrutas/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace sortearFrutas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonGirar = new System.Windows.Forms.Button();
+            this.buttonParar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonGirar
+            // 
+            this.buttonGirar.Location = new System.Drawing.Point(268, 362);
+            this.buttonGirar.Name = "buttonGirar";
+            this.buttonGirar.Size = new System.Drawing.Size(75, 23);
+            this.buttonGirar.TabIndex = 3;
+            this.buttonGirar.Text = "&Girar";
+            this.buttonGirar.UseVisualStyleBackColor = true;
+            this.buttonGirar.Click += new System.EventHandler(this.buttonGirar_Click);
+            // 
+            // buttonParar
+            // 
+            this.buttonParar.Location = new System.Drawing.Point(471, 362);
+            this.buttonParar.Name = "buttonParar";
+            this.buttonParar.Size = new System.Drawing.Size(75, 23);
+            this.buttonParar.TabIndex = 4;
+            this.buttonParar.Text = "&Parar";
+            this.buttonParar.UseVisualStyleBackColor = true;
+            this.buttonParar.Click += new System.EventHandler(this.buttonParar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(236, 465);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(344, 120);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::sortearFrutas.Properties.Resources.flor;
+            this.pictureBox2.Location = new System.Drawing.Point(369, 481);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 91);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::sortearFrutas.Properties.Resources.cogumelo;
+            this.pictureBox1.Location = new System.Drawing.Point(249, 481);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(85, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = global::sortearFrutas.Properties.Resources.sino;
+            this.pictureBox3.Location = new System.Drawing.Point(484, 481);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(85, 91);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 55F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(807, 103);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "CASSINO DO LUIGI";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Pontos: 25";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(811, 617);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.buttonParar);
+            this.Controls.Add(this.buttonGirar);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonGirar;
+        private System.Windows.Forms.Button buttonParar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ sortearFrutas/sortearFrutas/sortearFrutas/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortearFrutas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int num1;
+        int num2;
+        int num3;
+        int ponto = 25;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random gera1 = new Random();
+            num1 = gera1.Next(1,5);
+
+            if (num1 == 1)
+            {
+                pictureBox1.Image = Properties.Resources.cogumelo;
+            }
+            else if (num1 == 2)
+            {
+                pictureBox1.Image = Properties.Resources.venenoso;
+            }
+            else if (num1 == 3)
+            {
+                pictureBox1.Image = Properties.Resources.sino;
+            }
+            else if (num1 == 4)
+            {
+                pictureBox1.Image = Properties.Resources.estrela;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.flor;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random gera2 = new Random();
+            num2 = gera2.Next(1, 5);
+
+            if (num2 == 1)
+            {
+                pictureBox2.Image = Properties.Resources.cogumelo;
+            }
+            else if (num2 == 2)
+            {
+                pictureBox2.Image = Properties.Resources.venenoso;
+            }
+            else if (num2 == 3)
+            {
+                pictureBox2.Image = Properties.Resources.sino;
+            }
+            else if (num2 == 4)
+            {
+                pictureBox2.Image = Properties.Resources.estrela;
+            }
+            else 
+            {
+                pictureBox2.Image = Properties.Resources.flor;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        { 
+            Random gera3 = new Random();
+            num3 = gera3.Next(1, 5);
+
+            if (num3 == 1)
+            {
+                pictureBox3.Image = Properties.Resources.cogumelo;
+            }
+            else if (num3 == 2)
+            {
+                pictureBox3.Image = Properties.Resources.venenoso;
+            }
+            else if (num3 == 3)
+            {
+                pictureBox3.Image = Properties.Resources.sino;
+            }
+            else if (num3 == 4)
+            {
+                pictureBox3.Image = Properties.Resources.estrela;
+            }
+            else
+            {
+                pictureBox3.Image = Properties.Resources.flor;
+            }
+        }
+        private void buttonGirar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+
+            ponto -= 5;
+            label2.Text = "Pontos: " + ponto.ToString();
+
+            buttonGirar.Enabled = false;
+            buttonParar.Enabled = true;
+
+        }
+
+        private void buttonParar_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+
+            buttonGirar.Enabled = true;
+            buttonParar.Enabled = false;
+
+            if (num1 == num2 && num3 == num2 && num3 == 1)
+            {
+                ponto += 25;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+            if (num1 == num2 && num3 == num2 && num3 == 2)
+            {
+                ponto = 0;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+            if (num1 == num2 && num3 == num2 && num3 == 3)
+            {
+                ponto += 50;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+            if (num1 == num2 && num3 == num2 && num3 == 4)
+            {
+                ponto += 100;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+            if (num1 == num2 && num3 == num2 && num3 == 5)
+            {
+                ponto += 50;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+
+            if (ponto == 0)
+            {
+                MessageBox.Show("Você perdeu, Tente novamente", "Perdeu", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                ponto += 25;
+                label2.Text = "Pontos: " + ponto.ToString();
+            }
+        }
+    }
+}
